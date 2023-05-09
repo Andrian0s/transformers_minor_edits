aaab19c7000af4e86cadac0d8a1b3e3c06ad6d0d
ydshieh <ydshieh@users.noreply.github.com>
2023-05-09 16:07:55 +0200

    no tf-cpu

--- .circleci/create_circleci_config.py
+++ .circleci/create_circleci_config.py
@@ -178,6 +178,7 @@ tf_job = CircleCIJob(
         "pip install --upgrade pip",
         "pip install .[sklearn,tf-cpu,testing,sentencepiece,tf-speech,vision]",
         "pip install tensorflow_probability",
+        "pip uninstall -y tensorflow-cpu",
     ],
     parallelism=1,
     pytest_options={"rA": None},
