87654af990f4a608592a23aac3570eda3acbcdde
ydshieh <ydshieh@users.noreply.github.com>
2023-05-09 10:03:06 +0200

    run

--- src/transformers/models/bert/modeling_bert.py
+++ src/transformers/models/bert/modeling_bert.py
@@ -1107,7 +1107,6 @@ class BertForPreTraining(BertPreTrainedModel):
         ```python
         >>> from transformers import AutoTokenizer, BertForPreTraining
         >>> import torch
-        >>> import Hello
 
         >>> tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
         >>> model = BertForPreTraining.from_pretrained("bert-base-uncased")
